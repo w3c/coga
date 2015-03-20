ea0ea018b14cb42b79269dbd1031d585d3ebdaba
Michael Cooper <cooper@w3.org>
2015-03-20 11:38:44 -0400

    Moving bibliography to separate file

---
+++ common/biblio.js
@@ -0,0 +1,59 @@
+var biblio = {
+	"WAI-ARIA": {
+		"title": "Accessible Rich Internet Applications (WAI-ARIA) 1.1",
+		"href": "http://www.w3.org/TR/wai-aria-1.1/",
+		"status": "WD",
+		"publisher": "W3C",
+		"authors":
+		[
+			"James Craig",
+			"Michael Cooper",
+			"Shane McCarron"
+		],
+		"etAl": true,
+		"deliveredBy": "http://www.w3.org/WAI/PF/",
+	},
+	"Friston-1": {
+		"title": "Statistical parametric maps in functional imaging: a general linear approach",
+		
+		"publisher": "Wiley Subscription Services, Inc., A Wiley Company",
+		"authors": [
+			"Friston, K. J.",
+			"Ashburner, J.",
+			"Frith, C. D",
+			"Poline, J.-B.",
+			"Heather,J. D.",
+			"Frackowiak, R. S. J."
+		],
+		"etAl": true,
+		//"pages": "165–189",
+		// "journal": "Human Brain Mapping 2",
+		"publishDate": "1995",
+	},
+	"Talairach-1": {
+		"title": "Coplanar Stereotaxic Atlas of the Human Brain. Three-Dimensional Proportional System: An Approach to Cerebral Imaging",
+		"publisher": "Thieme, New York",
+		"authors": [
+			"Talairach, J.",
+			"Tournoux, P."
+		],
+		"etAl": false,
+		"publishDate": "1988",
+	},
+	"Eden-1": {
+		"title": "Abnormal processing of visual motion in dyslexia revealed by functional brain imaging.",
+		"publisher": " Macmillan Publishers LTD",
+		"authors": [
+			"Eden, G. F",
+			"VanMeter, J. W.",
+			"Rumsey, J. M.",
+			"Maisog, J. M.",
+			"Woods, R. P.",
+			"Zeffiro, T. A."
+		],
+		"etAl": true,
+		//"pages": "66–69",
+		// "journal": "Nature (London) 389",
+		"publishDate": "1996",
+	},
+};
