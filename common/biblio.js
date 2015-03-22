d3b6e70e4d793e8a8482383e8f43474a41342c5b
ayelet.s <ayelet.projects1@gmail.com>
2015-03-22 22:45:45 +0200

    update to biblio & some of the citations for dyslexia

--- common/biblio.js
+++ common/biblio.js
@@ -1,4 +1,124 @@
 var biblio = {
+	"Benson-1": {
+		"title": "The Neurology of Thinking",
+		"publisher": "Oxford Univ. Press",
+		"authors": "Benson, D. F.",
+		"etAl": false,
+		"publishDate": "1994",
+		},
+		
+		"Black-1": {
+			"title": "Localization and Neuro-imaging in Neuropsychology",
+			"publisher": "Academic Press",
+			"editors":"Kertesz, A.",
+			"authors": [
+			"Black, S. E.",
+			"Behrmann, M.",
+			],
+			"etAl": false,
+			//"pages": " 331–376",
+			"publishDate": "1994",
+			},
+			
+			"Dejerine-1": {
+				"title": "Sur un cas de cécité verbale avec agraphie, suivi d’autopsie",
+				"publisher": "United States National Academy of Sciences",
+				"authors":"Dejerine, J.",
+				"etAl": false,
+				//"pages": "197–201",
+				// "journal": "C. R. Societe du Biologie 43",
+				"publishDate": "1891",
+				},
+		
+	"Eden-1": {
+		"title": "Abnormal processing of visual motion in dyslexia revealed by functional brain imaging.",
+		"publisher": " Macmillan Publishers LTD",
+		"authors": [
+			"Eden, G. F",
+			"VanMeter, J. W.",
+			"Rumsey, J. M.",
+			"Maisog, J. M.",
+			"Woods, R. P.",
+			"Zeffiro, T. A."
+		],
+		"etAl": false,
+		//"pages": "66–69",
+		// "journal": "Nature (London) 389",
+		"publishDate": "1996",
+	},
+	"Friedman-1": {
+		"publisher": "Oxford Univ. Press, New York",
+		"authors": [
+		"Friedman, R. F.",
+		"Ween, J. E.",
+		"Albert, M. L.",
+		],
+		"editors":[
+		"Heilman, K. M.",
+		"Valenstein, E.",
+		],
+		"etAl": false,
+		//"pages": "37–62",
+		// "journal": "Clinical Neuropsychology",
+		"publishDate": "1993",
+		},
+	
+	"Friston-1": {
+		"title": "Statistical parametric maps in functional imaging: a general linear approach",
+		
+		"publisher": "Wiley Subscription Services, Inc., A Wiley Company",
+		"authors": [
+			"Friston, K. J.",
+			"Ashburner, J.",
+			"Frith, C. D",
+			"Poline, J.-B.",
+			"Heather,J. D.",
+			"Frackowiak, R. S. J."
+		],
+		"etAl": false,
+		//"pages": "165–189",
+		// "journal": "Human Brain Mapping 2",
+		"publishDate": "1995",
+	},
+	"Geschwind-1": {
+		"title": "Disconnexion syndromes in animals and man. I.",
+		"publisher": "Wiley Subscription Services, Inc., A Wiley Company",
+		"authors": "Geschwind, N. ",
+		"etAl": false,
+		//"pages": " 237–2949",
+		// "journal": "Brain 88",
+		"publishDate": "1965",
+		},
+		"Henderson-1": {
+		"title": "Anatomy of posterior pathways in reading: A reassessment",
+		"publisher": " Macmillan Publishers LTD",
+		"authors": "Henderson, V. W.",
+		"etAl": false,
+		//"pages": "119–133",
+		// "journal": "Brain Lang. 29",
+		"publishDate": "1986",
+	},
+	
+	"Talairach-1": {
+		"title": "Coplanar Stereotaxic Atlas of the Human Brain. Three-Dimensional Proportional System: An Approach to Cerebral Imaging",
+		"publisher": "Thieme, New York",
+		"authors": [
+			"Talairach, J.",
+			"Tournoux, P."
+		],
+		"etAl": false,
+		"publishDate": "1988",
+	},
+	"Shaywitz-1": {
+		"title": "Functional disruption in the organization of the brain for reading in dyslexia",
+		"publisher": "United States National Academy of Sciences",
+		"authors":"S Shaywitz.",
+		"etAl": false,
+		//"pages": "2636–2641",
+		// "journal": "Proc. Natl. Acad. Sci. USA Vol. 95",
+		"publishDate": "1998",
+		},
+
 	"WAI-ARIA": {
 		"title": "Accessible Rich Internet Applications (WAI-ARIA) 1.1",
 		"href": "http://www.w3.org/TR/wai-aria-1.1/",
@@ -13,47 +133,5 @@ var biblio = {
 		"etAl": true,
 		"deliveredBy": "http://www.w3.org/WAI/PF/",
 	},
-	"Friston-1": {
-		"title": "Statistical parametric maps in functional imaging: a general linear approach",
-		
-		"publisher": "Wiley Subscription Services, Inc., A Wiley Company",
-		"authors": [
-			"Friston, K. J.",
-			"Ashburner, J.",
-			"Frith, C. D",
-			"Poline, J.-B.",
-			"Heather,J. D.",
-			"Frackowiak, R. S. J."
-		],
-		"etAl": true,
-		//"pages": "165–189",
-		// "journal": "Human Brain Mapping 2",
-		"publishDate": "1995",
-	},
-	"Talairach-1": {
-		"title": "Coplanar Stereotaxic Atlas of the Human Brain. Three-Dimensional Proportional System: An Approach to Cerebral Imaging",
-		"publisher": "Thieme, New York",
-		"authors": [
-			"Talairach, J.",
-			"Tournoux, P."
-		],
-		"etAl": false,
-		"publishDate": "1988",
-	},
-	"Eden-1": {
-		"title": "Abnormal processing of visual motion in dyslexia revealed by functional brain imaging.",
-		"publisher": " Macmillan Publishers LTD",
-		"authors": [
-			"Eden, G. F",
-			"VanMeter, J. W.",
-			"Rumsey, J. M.",
-			"Maisog, J. M.",
-			"Woods, R. P.",
-			"Zeffiro, T. A."
-		],
-		"etAl": true,
-		//"pages": "66–69",
-		// "journal": "Nature (London) 389",
-		"publishDate": "1996",
-	},
+	
 };
