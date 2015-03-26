9c98d64f1378d3f2b2e98cf3755496c725d4256c
ayelet.s <ayelet.projects1@gmail.com>
2015-03-26 18:43:10 +0200

    another update to the dyslexia

--- common/biblio.js
+++ common/biblio.js
@@ -7,6 +7,19 @@ var biblio = {
 		"publishDate": "1994",
 		},
 		
+		"Berninger-1": {
+		"title": "A Multidisciplinary Approach to Understanding Developmental Dyslexia Within Working-Memory Architecture: Genotypes, Phenotypes, Brain, and Instruction",
+
+		"publisher": "Developmental Neuropsychology 33 (6)",
+		"authors": [
+			"Berninger, B.",
+			"Virginia W."
+		],
+		"etAl": true,	
+		"publishDate": "2008",
+		//"pages": "704-744"
+	},
+		
 		"Black-1": {
 			"title": "Localization and Neuro-imaging in Neuropsychology",
 			"publisher": "Academic Press",
@@ -16,23 +29,53 @@ var biblio = {
 			"Behrmann, M.",
 			],
 			"etAl": false,
-			//"pages": " 331–376",
+			//"pages": " "331–376",
 			"publishDate": "1994",
 			},
 			
+			"Cao-1": {
+		"title": "Deficient orthographic and phonological representations in children with dyslexia revealed by brain activation patterns",
+		"href": "http://onlinelibrary.wiley.com/doi/10.1111/j.1469-7610.2006.01684.x/abstract;jsessionid=0C68D8EFA292BDC2CD782A68E41AD2D4.f03t03",
+		"publisher": "Journal of Child Psychology and Psychiatry, Volume 47, Issue 10",
+		"authors":
+		[
+			"Fan Cao",
+			"Tali Bitan",
+			"Tai-Li Chou"
+		],
+		"etAl": true,
+                //"pages": "1041–1050",
+                "publishDate": "2006",
+		},
+			
 			"Dejerine-1": {
 				"title": "Sur un cas de cécité verbale avec agraphie, suivi d’autopsie",
-				"publisher": "United States National Academy of Sciences",
+				"publisher": "C. R. Societe du Biologie 43",
 				"authors":"Dejerine, J.",
 				"etAl": false,
 				//"pages": "197–201",
-				// "journal": "C. R. Societe du Biologie 43",
 				"publishDate": "1891",
 				},
+				
+			"Demonet-1": {
+		"title": "A PET study of cognitive strategies in normal subjects during language tasks influence of phonetic ambiguity and sequence processing on phoneme monitoring",
+
+		"publisher": "Brain 117",
+		"authors": [
+			"Demonet, J. F.",
+			"Price, C.",
+			"Wise., R.",
+			"Frackowiak, R. S. J."
+		],
+		"etAl": false,
+		//"pages": "671–682",
+		// "publishedBy": "Oxford University Press",
+		"publishDate": "1994",
+	},
 		
 	"Eden-1": {
 		"title": "Abnormal processing of visual motion in dyslexia revealed by functional brain imaging.",
-		"publisher": " Macmillan Publishers LTD",
+		"publisher": "Nature (London) 389",
 		"authors": [
 			"Eden, G. F",
 			"VanMeter, J. W.",
@@ -43,10 +86,12 @@ var biblio = {
 		],
 		"etAl": false,
 		//"pages": "66–69",
-		// "journal": "Nature (London) 389",
+		// "publishedBy": "Macmillan Publishers LTD",
 		"publishDate": "1996",
 	},
+	
 	"Friedman-1": {
+		"title":"Clinical Neuropsychology",
 		"publisher": "Oxford Univ. Press, New York",
 		"authors": [
 		"Friedman, R. F.",
@@ -59,14 +104,13 @@ var biblio = {
 		],
 		"etAl": false,
 		//"pages": "37–62",
-		// "journal": "Clinical Neuropsychology",
 		"publishDate": "1993",
 		},
 	
 	"Friston-1": {
 		"title": "Statistical parametric maps in functional imaging: a general linear approach",
 		
-		"publisher": "Wiley Subscription Services, Inc., A Wiley Company",
+		"publisher": "Human Brain Mapping, Volume 2, Issue 4",
 		"authors": [
 			"Friston, K. J.",
 			"Ashburner, J.",
@@ -77,26 +121,129 @@ var biblio = {
 		],
 		"etAl": false,
 		//"pages": "165–189",
-		// "journal": "Human Brain Mapping 2",
+		// "journal": "Wiley Subscription Services, Inc., A Wiley Company",
 		"publishDate": "1995",
 	},
+	
+	
+    "Galaburda-1": {
+		"title": "Dyslexia and Development: Neurobiological Aspects of Extra-ordinary Brains, Part 998",
+
+		"publisher": "Harvard University Press",
+		"authors": "Galaburda, A. M.",
+		"etAl": false,	
+		"publishDate": "1993",
+	},
+	
+	
 	"Geschwind-1": {
 		"title": "Disconnexion syndromes in animals and man. I.",
-		"publisher": "Wiley Subscription Services, Inc., A Wiley Company",
+		"publisher": "Brain 88",
 		"authors": "Geschwind, N. ",
 		"etAl": false,
 		//"pages": " 237–2949",
-		// "journal": "Brain 88",
+		// "publishedBy": "Wiley Subscription Services, Inc., A Wiley Company",
 		"publishDate": "1965",
 		},
+		
 		"Henderson-1": {
 		"title": "Anatomy of posterior pathways in reading: A reassessment",
-		"publisher": " Macmillan Publishers LTD",
+		"publisher": "Brain Lang. 29",
 		"authors": "Henderson, V. W.",
 		"etAl": false,
 		//"pages": "119–133",
-		// "journal": "Brain Lang. 29",
+		// "publishedBy": "Macmillan Publishers LTD",
 		"publishDate": "1986",
+	},
+	
+	"Lyon-1": {
+		"title": "Neuroimaging a window to the neurological foundations of learning and behavior in children",
+
+		"publisher": "Brookes Publishing Company",
+		"authors": [
+			"Lyon, G.",
+			"Rumsey J.",
+			"Constable, R. T.",
+			"Shaywitz, S."
+		],
+		"etAl": false,
+		//"pages": "58-73",
+	
+		"publishDate": "1996",
+	},
+
+
+	
+	"Paulesu-1": {
+		"title": "Is developmental dyslexia a disconnection syndrome?",
+		
+		"publisher": "Brain 119",
+		"authors": [
+			"Paulesu, E.",
+			"Frith, U.",
+			"Snowling, M.",
+			"Gallagher, A.",
+			"Morton, J.",
+			"Frackowiak, R. S. J.",
+			"Frith, C. D."
+		],
+		"etAl": false,
+		//"pages": "143–157",
+	
+		"publishDate": "1996",
+	},
+	
+	"Petersen-1": {
+		"title": "Activation of extrastriate and frontal cortical areas by visual words and word-like stimuli",
+
+		"publisher": "Science, Volume 249, Issue 4972",
+		"authors": [
+			"Petersen, S. E.",
+			"Fox, P. T.",
+			"Snyder, A. Z.",
+			"Raichle, M. E."
+		],
+		"etAl": false,
+		//"pages": "1041–1044",
+		// "publishedBy": "American Association for the Advancement of Science",
+		"publishDate": "1990",
+	},
+
+
+
+"Pugh-1": {
+		"title": "Cerebral organization of component processes in reading",
+
+		"publisher": "Brain 119",
+		"authors": [
+			"Pugh, K.",
+			"Shaywitz, B.",
+			"Constable, R. T.",
+			"Shaywitz, S."
+		],
+		"etAl": true,
+		//"pages": "1221–1238",
+		// "publishedBy": "Oxford University Press",
+		"publishDate": "1996",
+	},
+
+	
+	"Rumsey-1": {
+		"title": "Failure to activate the left temporal cortex in dyslexia: insights from a multiple case study of dyslexic adults",
+		
+		"publisher": "Arch. Neurol. 54",
+		"authors": [
+			"Rumsey, J. M.",
+			"Nace, K.",
+			"Donohue, B.",
+			"Wise, D.",
+			"Maisog, J. M.",
+			"Andreason, P."
+		],
+		"etAl": false,
+		//"pages": "562–573",
+	
+		"publishDate": "1997",
 	},
 	
 	"Talairach-1": {
@@ -109,14 +256,35 @@ var biblio = {
 		"etAl": false,
 		"publishDate": "1988",
 	},
+	
+	"Tallal-1": {
+		"title": "Temporal information processing in the nervous system: special reference to dyslexia and dysphasia",
+
+		"publisher": "Annals of the New York Academy of Sciences, Vol. 682",
+		"authors": "Tallal, P.",
+		"etAl": true,	
+		"publishDate": "1993",
+	},
+	
+	
 	"Shaywitz-1": {
 		"title": "Functional disruption in the organization of the brain for reading in dyslexia",
-		"publisher": "United States National Academy of Sciences",
-		"authors":"S Shaywitz.",
-		"etAl": false,
+		"publisher": "Proc. Natl. Acad. Sci. USA Vol. 95",
+		"authors":"Shaywitz S.",
+		"etAl": true,
 		//"pages": "2636–2641",
-		// "journal": "Proc. Natl. Acad. Sci. USA Vol. 95",
+		// "publishedBy": "United States National Academy of Sciences",
 		"publishDate": "1998",
+		},
+		
+
+"Stein-1": {
+		"title": "The magnocellular theory of developmental dyslexia",
+		"href": "http://www.ncbi.nlm.nih.gov/pubmed/11305228",
+		"publisher": "Dyslexia. 2001 Jan-Mar;7(1)",
+		"authors": "Stein J.",
+		"etAl": false,
+                //"pages": "12-36",
 		},
 
 	"WAI-ARIA": {
