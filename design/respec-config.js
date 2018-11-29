fc24bea9051f5e8f4e2c42db10eedc44970ffa44
ruoxiran <ran@w3.org>
2018-11-29 15:17:38 +0800

    change the short name

--- design/respec-config.js
+++ design/respec-config.js
@@ -15,7 +15,7 @@ var respecConfig = {
     //diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
 
     // the specifications short name, as in http://www.w3.org/TR/short-name/
-    shortName:            "coga-usable-requirement",
+    shortName:            "coga-usable-design",
 
 
     // if you wish the publication date to be other than today, set this
