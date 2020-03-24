6ca350bbae984daa85d860098c0b442d2fb4046b
ruoxiran <ran@w3.org>
2020-03-24 17:09:09 +0800

    prepared the WD

--- content-usable/respec-config.js
+++ content-usable/respec-config.js
@@ -7,7 +7,7 @@ var respecConfig = {
     permalinkHide:     false,
     tocIntroductory: true,
     // specification status (e.g., WD, LC, NOTE, etc.). If in doubt use ED.
-    specStatus:           "ED",
+    specStatus:           "WD",
     noRecTrack: true,
     //crEnd:                "2012-04-30",
     //perEnd:               "2013-07-23",
@@ -20,13 +20,12 @@ var respecConfig = {
 
     // if you wish the publication date to be other than today, set this
     //publishDate:  "2017-05-09",
-    copyrightStart:  "2018",
-    license: "document",
+    copyrightStart:  "2020",
 
     // if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
     // and its maturity status
-    //previousPublishDate:  "",
-    //previousMaturity:  "",
+    previousPublishDate:  "2019-07-16",
+    previousMaturity:  "WD",
     //prevRecURI: "",
     //previousDiffURI: "",
 
@@ -45,15 +44,29 @@ var respecConfig = {
         mailto: "lisa.seeman@zoho.com",
         w3cid: 16320
       },
+      {
+        name: "Rachael Montgomery",
+        mailto: "rachael@accessiblecommunity.org",
+        company: "Invited expert",
+        w3cid: 90310
+      },
        
       {
-        name: "Michael Cooper",
+        name: "Steve Lee",
         url: 'https://www.w3.org',
-        mailto: "cooper@w3.org",
+        mailto: "stevelee@w3.org",
         company: "W3C",
         companyURI: "http://www.w3.org",
-        w3cid: 34017
-      }
+        w3cid: 71103
+      },
+      {
+        name: "Ruoxi Ran",
+        url: 'https://www.w3.org',
+        mailto: "ran@w3.org",
+        company: "W3C",
+        companyURI: "http://www.w3.org",
+        w3cid: 100586
+      },
       
     ],
 
